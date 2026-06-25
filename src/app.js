1d62541f9b4d62baab32f88e07eed578f99e47e2
mamun007molla <majanfermun@gmail.com>
2026-06-25 23:37:02 +0600

    auth route added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import express from "express";
+
+/* all import routes */
+import { router as authRoutes } from "./routes/auth.routes.js";
 export const app = express();
 app.use(express.json());
+/* using all the routes here */
+app.use("/api/auth",authRoutes)
+
