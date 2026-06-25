bddbf16008bb0f7f6a76caed5ab9e48247921f18
mamun007molla <majanfermun@gmail.com>
2026-06-25 23:31:49 +0600

    add db connector

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import { app } from "./src/app.js";
+import { dbConnect } from "./src/config/database.js";
 
+
+dbConnect()
 app.listen(3000, () => {
   console.log("server is running on http://localhost:3000");
 });
